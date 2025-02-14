acbef322c327b1fb680e7c6db00674584cf488e0
BIniyam Getachew Teka <biniyam.getachew@aastustudent.edu.et>
2025-02-14 23:19:00 +0300

    BiniyamGetachew1001
    elcome to my portfolio! I’m Biniyam Getachew, a passionate video editor and motion graphics designer dedicated to crafting engaging visual stories. My work blends creativity with technical precision, transforming ideas into compelling motion visuals.
    
    This website showcases my expertise in noir-style cinematics, dynamic video editing, and motion design tailored for branding, storytelling, and marketing. Whether you're looking for high-impact promotional content or cinematic motion sequences, you'll find a curated selection of my best projects here.
    
    Let’s create something remarkable together!

---
+++ Biniyam Getachew/assets/script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.querySelector('.navbar');
+
+    menuIcon.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            navbar.classList.remove('active');
+        }
+    });
+});
+
+window.addEventListener('load', function() {
+    const images = document.querySelectorAll('img');
+    images.forEach(img => {
+        img.loading = 'lazy';
+    });
+});
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
